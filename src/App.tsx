8bbe68124332335218cad674f14f9b7f70746af7
edcastillob <edwar.castillo@gmail.com>
2024-01-21 11:36:47 -0400

    Router, config de rutas en el app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { Button, Container } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { AppRouter } from './Router'
 
 function App() {
   
 
   return (
-    <>
-      <Container maxWidth="xl">
-        <Button variant='contained'>Start Site</Button>
-      </Container>
-    </>
+  <BrowserRouter>
+  <AppRouter />
+  </BrowserRouter>
   )
 }
 
