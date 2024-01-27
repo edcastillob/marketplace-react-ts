d99b36d0c2c622d37ed6e2814985ed1b81e4d21f
edcastillob <edwar.castillo@gmail.com>
2024-01-27 17:33:37 -0400

    Add to cart

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,21 +7,43 @@ import {
   Divider,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { addToCart } from "../../redux/slices/cart.slice";
 
 
 type CardProps ={ 
   id: string | number;
   name: string;
-  category:string;
+  description:string;
   price: number;
   image: string;
+  category: string
  
 }
 
-export const CardComponent: React.FC<CardProps> = ({name, category, price, image, id}) => {
+export const CardComponent: React.FC<CardProps> = ({name, description, price, image, id, category}) => {
+
+  const [disableOptCartBtn, setDisableOptCartBtn] = useState<boolean>(false);
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const productExist = useAppSelector((state) => (state.cartReducer));
+
+  const handleAddToCart = () => { 
+      dispatch(addToCart({
+        id, 
+        name, 
+        price,
+        image, 
+        description,
+      }))
+  }
+
+  useEffect(() => {     
+    setDisableOptCartBtn( productExist.some((p) =>( p.id === id)) )      
+  }, [productExist, id])
+  
 
   return (
     <Card sx={{boxShadow:"rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset"}}>
@@ -50,6 +72,9 @@ export const CardComponent: React.FC<CardProps> = ({name, category, price, image
       <CardActions>
         <Button variant="contained" size="small" fullWidth onClick={() => navigate(`/product/${id}`)}>
           Detail
+        </Button>
+        <Button disabled={disableOptCartBtn} variant="outlined" size="small" fullWidth onClick={handleAddToCart}>
+          Add to Cart
         </Button>
       </CardActions>
     </Card>
--- src/redux/slices/cart.slice.ts
+++ src/redux/slices/cart.slice.ts
@@ -1,22 +1,33 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface CartState {
+interface CartAddState {
     id: string | number;
     name: string;
     image: string;
     price: number;
     description: string;
 }
+interface CartRemoveState {
+    id: string | number;  
+}
 
-const initialState: CartState[] = [];
+const initialState: CartAddState[] = [];
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, actions: PayloadAction<CartState>) => { },
-    removeToCart: (state, actions: PayloadAction<CartState>) => { },
+    addToCart: (state, actions: PayloadAction<CartAddState>) => { 
+      const { id } = actions.payload;
+      if( 
+        state.length === 0 || 
+        state.filter((item) =>  item.id === id).length === 0
+       ){
+        state.push(actions.payload)
+       }
+    },
+    removeToCart: (state, actions: PayloadAction<CartRemoveState>) => { },
   },
 });
 
-export const {} = cartSlice.actions;
+export const { addToCart, removeToCart } = cartSlice.actions;
